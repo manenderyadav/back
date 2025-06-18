53b06d35d7f0e7fe83374e60a6c64312b053d64e
manenderyadav <136232443+manenderyadav@users.noreply.github.com>
2025-06-18 10:27:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ io.on('connection', (socket) => {
 });
 
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
